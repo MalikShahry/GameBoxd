019a668cbaf0caf6de6b38e55c2ce20cbe97105c
MalikShahry <malikshahry23@gmail.com>
2025-07-22 18:18:18 -0600

    Communication between frontend and backend established

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+// Fetch hello from the server side
+  useEffect(() => {
+    fetch('http://localhost:8080/api/hello')
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
