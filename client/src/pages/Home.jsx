90dbb6706e93219320cafd88147e98878151340d
MalikShahry <malikshahry23@gmail.com>
2025-07-28 17:48:41 -0600

    Added RAWG API to code + fixed up divs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../styles/Home.css'
 import backgroundImage from '../assets/nightreign.webp'
 
+
 function Home() {
+    
+    const API_KEY = '843f0e17e8164a86acbbff609402c1fb';
+    const getGamesURL = `https://api.rawg.io/api/games?key=${API_KEY}`;
+
+    const [games, setFeaturedGames] = useState([]);
+
+
+    // Pull from RAWG API, return the first 5 games from the RAWG db
+    useEffect(() => {
+        axios.get(getGamesURL)
+        .then(response => {
+            setFeaturedGames(response.data.results.slice(0,5));
+        })
+        .catch(error => {
+            console.error(error)
+        });
+    }, []);
 
 
     return(
@@ -23,11 +42,19 @@ function Home() {
                     <br/>
                     Show the world your favourite games.
                     </h2>              
-            </div>
+                </div>
 
             <section class='Featured'>
+                <ul class='poster'>
+                    {games.map(game => (
+                        <li key={game.id}>{game.name}</li>
+                    ))}
 
+                    <div>
 
+                    </div>
+
+                </ul>
             </section>
 
         </div>
